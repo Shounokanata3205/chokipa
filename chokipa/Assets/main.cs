060423053f5904567fe480a76d378629f1855f54
241027 <kahonosatann@gmail.com>
2025-10-06 13:33:38 +0900

    ボタンの諸々

--- chokipa/Assets/main.cs
+++ chokipa/Assets/main.cs
@@ -4,5 +4,60 @@ using UnityEngine;
 
 public class main : MonoBehaviour
 {
-    public GameObject menu,;
+    public GameObject MenuTag,
+        Menu,
+        Touch,
+        Setting,
+        TouchMenu,
+        Caress,
+        TakeOut,
+        SettingMenu,
+        Volume,
+        Credit,
+        Back;
+
+    public void MenuTagButton()
+    {
+        Menu.SetActive(true);
+        MenuTag.SetActive(false);
+    }
+
+    public void TouchButton()
+    {
+        TouchMenu.SetActive(true);
+        Back.SetActive(true);
+        Menu.SetActive(false);
+    }
+
+    public void SettingButton()
+    {
+        SettingMenu.SetActive(true);
+        Back.SetActive(true);
+        Menu.SetActive(false);
+    }
+
+    public void BackButton()
+    {
+        Menu.SetActive(true);
+    }
+
+    public void CaressButton()
+    {
+        //SettingMenu.SetActive(false);
+    }
+
+    public void TakeOutButton()
+    {
+        //SettingMenu.SetActive(false);
+    }
+
+    public void VolumeButton()
+    {
+        //SettingMenu.SetActive(false);
+    }
+
+    public void CreditButton()
+    {
+        //SettingMenu.SetActive(false);
+    }
 }
