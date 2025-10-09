842602db9d8134acee94ed330540482001bc9054
241018 <c241018@ivy.ac.jp>
2025-10-09 13:24:37 +0900

    タイトルシーン遷移変更修正

---
+++ chokipa/Assets/Scenes/チョキぶた素材/FadeManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FadeManager : MonoBehaviour
+{
+    [SerializeField] CanvasGroup CanvasGroup;
+
+
+    //シングルトンのコード開始
+    static public FadeManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    //シングルトンのコード終了
+
+    //フェードインフェードアウトの実装
+    //明るくなる
+    public void FadeIn()
+    {
+        CanvasGroup.DOFade(0, 1.5f);
+    }
+    public void FadeOut()
+    {
+        CanvasGroup.DOFade(1, 1.5f);
+    }
+    public void FadeOutToIn(TweenCallback action = null)
+    {
+        CanvasGroup.DOFade(1, 1.5f).OnComplete(
+             () =>
+             {
+                action();
+                FadeIn();
+             });
+    }
+}
---
+++ chokipa/Assets/Scenes/チョキぶた素材/TitleManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+
+public class TitleManager : MonoBehaviour
+
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnTransitionToMain()
+    {
+        FadeManager.Instance.FadeOutToIn(SceneToMain);
+    }
+    public void OnTransitionToEnd()
+    {
+        FadeManager.Instance.FadeOutToIn(SceneToEnd);
+    }
+    public void OnTransitionToTitle()
+    {
+        FadeManager.Instance.FadeOutToIn(SceneToTitle);
+    }
+
+void SceneToMain()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+    void SceneToEnd()
+    {
+        SceneManager.LoadScene("End");
+    }
+    void SceneToTitle()
+    {
+        SceneManager.LoadScene("title");
+    }
+}
